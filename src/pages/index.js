5113e96c95820724d50d536e7899571d6e02f4a7
Larissa Perinoto <perinotolarissa@gmail.com>
2024-08-09 18:47:57 -0300

    style: insere folha de estilo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
   const [objects, setObjects] = useState([]);
 
   return (
-    <main>
-      <div>
-        <div id="heatmapContainer"></div>
+    <main className={styles.main}>
+      <div className={styles.container_1}>
+        <div id="heatmapContainer" className={styles.heatmapContainer}></div>
       </div>
 
-      <div>
-        <div>
+      <div className={styles.container_2}>
+        <div className={styles.inputContainer}>
           <label htmlFor="input">Selecione uma imagem</label>
           <input id="input" type="file" accept="image/*" />
         </div>
-        <div>
-          <button>Criar mapa de calor</button>
+        <div className={styles.createButtonContainer}>
+          <button className={styles.createButton}>Criar mapa de calor</button>
         </div>
 
         <div>
-          <div>
+          <div className={styles.selectContainer}>
             <label htmlFor="dropdown">Selecione o objeto</label>
             <select id="dropdown">
               <option value="todos" key="todos" defaultValue={true}>
@@ -32,7 +35,7 @@ export default function Home() {
               })}
             </select>
           </div>
-          <div>
+          <div className={styles.downloadContainer}>
             <button>Fazer download</button>
           </div>
         </div>
