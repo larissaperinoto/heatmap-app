3f92e30449c124f5999129615aa8bafe7a876723
Larissa Perinoto <perinotolarissa@gmail.com>
2024-08-09 18:33:03 -0300

    feat: cria função auxiliar para leitura de arquivo

---
+++ src/pages/utils/readFile.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+import path from "path";
+
+export function readFile() {
+  try {
+    const filePath = path.join(process.cwd(), "data.json");
+    const data = fs.readFileSync(filePath, "utf8");
+
+    if (!data) {
+      throw new Error("File is empty.");
+    }
+
+    return JSON.parse(data).hits.hits;
+  } catch (e) {
+    throw new Error("Cannot read the file: ", e);
+  }
+}
