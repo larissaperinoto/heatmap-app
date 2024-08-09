392bd81ffd7b6563063281ed92f10461dc63a1e3
Larissa Perinoto <perinotolarissa@gmail.com>
2024-08-09 18:35:35 -0300

    feat: insere rotas do servidor para obter dados do cache

---
+++ src/pages/api/heatmap/data.js
@@ -0,0 +1,30 @@
+import { httpStatus } from "../utils/statusHttp";
+import { calculateCentroidBoundingBox } from "../utils/boundingBox";
+import { readFile } from "../utils/readFile";
+
+export default async function handler(_req, res) {
+  try {
+    const response = readFile();
+
+    const data = response.reduce((acc, dado) => {
+      const formated = dado["fields"]["deepstream-msg"].map((item) => {
+        const [id, minX, minY, maxX, maxY, object, region] = item.split("|");
+        const { x, y } = calculateCentroidBoundingBox(
+          +minX,
+          +maxX,
+          +minY,
+          +maxY
+        );
+
+        return { x, y, id, object, region, value: 1 };
+      });
+
+      acc.push(...formated);
+      return acc;
+    }, []);
+
+    res.status(httpStatus.OK).json(data);
+  } catch (e) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json([]);
+  }
+}
---
+++ src/pages/api/heatmap/objects.js
@@ -0,0 +1,24 @@
+import { readFile } from "../utils/readFile";
+import { httpStatus } from "../utils/statusHttp";
+
+export default async function handler(_req, res) {
+  try {
+    const response = readFile();
+
+    const data = response.reduce((acc, dado) => {
+      dado["fields"]["deepstream-msg"].forEach((item) => {
+        const object = item.split("|")[5];
+
+        if (!acc.includes(object)) {
+          acc.push(object);
+        }
+      });
+
+      return acc;
+    }, []);
+
+    res.status(httpStatus.OK).json(data);
+  } catch (e) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json([]);
+  }
+}
